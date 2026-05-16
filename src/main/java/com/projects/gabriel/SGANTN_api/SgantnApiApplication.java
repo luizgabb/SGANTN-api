ebb4569f6e36ec4264a9d73fce387a22ec33c086
Luiz Gabriel <gabrielaquinostedile@gmail.com>
2026-05-16 13:36:22 -0300

    criacao do controller, erro de autorizacao do spring security que ainda nao foi resolvido

--- src/main/java/com/projects/gabriel/SGANTN_api/SgantnApiApplication.java
+++ src/main/java/com/projects/gabriel/SGANTN_api/SgantnApiApplication.java
@@ -2,8 +2,13 @@ package com.projects.gabriel.SGANTN_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
 public class SgantnApiApplication {
 
 	public static void main(String[] args) {
